28e604873fffe238f80f660beed87a91320b19c1
isiah-a <isiaha478@gmail.com>
2025-03-24 20:35:20 -0400

    finished math for stats

--- statzcw/stats.py
+++ statzcw/stats.py
@@ -1,4 +1,4 @@
-
+import math
 from typing import List
 
 def zcount(data: List[float]) -> float :
@@ -10,26 +10,61 @@ def zmean(data: List[float]) -> float :
     return avg
 
 def zmode(data: List[float]) -> float :
-    
+    return max(set(data), key=data.count)
 
 def zmedian(data: List[float]) -> float :
-    pass
+    d = len(data)
+    data.sort()
+    if d % 2 == 0:
+        median1 = data[d // 2]
+        median2 = data[d // 2 - 1]
+        median = (median1 + median2) / 2
+    else:
+        median  = data[d // 2]
+    return median
 
 def zvariance(data: List[float]) -> float :
-    pass
+    avg = sum(data) / len(data)
+    formula = sum((i - avg) ** 2 for i in data) / len(data)
+    return formula
 	
 def zstddev(data: List[float]) -> float :
-    # sqrt of variance
-    pass
+    avg = sum(data) / len(data)
+    variance = sum(pow(x - avg, 2) for x in data) / len(data)
+    sqrt = math.sqrt(variance)
+    return sqrt
+
 
 def zstderr(data: List[float]) -> float :
-    pass
+    avg = sum(data) / len(data)
+    variance = sum(pow(x - avg, 2) for x in data) / len(data)
+    stderr = variance ** 0.5
+    return stderr
 
 def cov(a, b):
-    pass
+    avg1 = a.sum() / len(b)
+    avg2 = b.sum() / len(a)
+    return sum([(x - avg1) * (y - avg2) for x, y in zip(a,b)]) / (len(a) - 1)
 
 def zcorr(datax: List[float], datay: List[float]) -> float :
-    pass
+    avg1 = sum(datax) / len(datax)
+    variance1 = sum(pow(x - avg1, 2) for x in datax) / len(datax)
+    datax_dev = math.sqrt(variance1)
+
+    #Mean and deviation of both lists...
+
+    avg2 = sum(datay) / len(datay)
+    variance2 = sum(pow(x - avg2, 2) for x in datay) / len(datay)
+    datay_dev = math.sqrt(variance2)
+
+    corr_numerator = 0.0
+    for i in range(len(datax)):
+        corr_numerator += ((datax[i] - avg1) * (datay[i] - avg2))
+    corr_denominator = datax_dev * datay_dev
+
+    corr = corr_numerator / corr_denominator
+    return corr
+
 
 
 def readDataFile(file):
